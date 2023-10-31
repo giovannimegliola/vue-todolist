80d2d5ef5a770fcfcc5e86fca16f0aae1325a848
giovannimegliola <giovanni.megliola@hotmail.it>
2023-10-31 17:21:06 +0100

    completed milestone 3

--- js/script.js
+++ js/script.js
@@ -6,14 +6,27 @@ createApp({
       tasks: [
         { text: 'Completa il milestone 1', done: true, id: 1 },
         { text: 'Completa il milestone 2', done: true, id: 2 },
-        { text: 'Completa il milestone 3', done: false, id: 3 },
+        { text: 'Completa il milestone 3', done: true, id: 3 },
         { text: 'Procedi con bonus 1', done: false, id: 4 },
         { text: 'Continua con bonus 2', done: false, id: 5 },
         { text: 'Trasforma questa lista scarna in una todo list più articolata', done: false, id: 5 }
-      ]
+      ],
+      newActivity: ''
     }
   },
   methods: {
+
+    addTask() {
+      if (this.newActivity !== '') {
+        this.tasks.push({
+          text: this.newActivity,
+          done: false, 
+          id: this.tasks.length + 1,
+        });
+        this.newActivity = '';
+      }
+    },
+
     removeTask(todoId) {
       const index = this.tasks.findIndex(todo => todo.id === todoId);
       if (index >= 0) {
